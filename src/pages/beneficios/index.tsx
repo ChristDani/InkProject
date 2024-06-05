bb6287491c7975474d1f3074ec5b280feb69d7e4
RoyerAI <racostai@unprg.edu.pe>
2024-06-05 10:35:16 -0500

    Beneficios

--- src/pages/beneficios/index.tsx
+++ src/pages/beneficios/index.tsx
@@ -1,8 +1,98 @@
 const Beneficios = () => {
     return (
         <>
-            <div className="red">
-                beneficios
+            <div className="text-[#002140] text-center w-1/2 mx-auto flex flex-col gap-4">
+                <h1 className="text-5xl font-bold">Beneficios</h1>
+                <span className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, beatae voluptatibus! Cupiditate veniam, amet libero commodi nam a saepe rerum enim deserunt.</span>
+            </div>
+            <div className=" flex flex-col gap-10 mt-12">
+                <div className="flex w-full justify-center">
+                    <div className="w-1/3">
+                        <span className="font-bold text-2xl italic text-[#002140]">Docentes</span>
+                        <p className="mt-3 text-[#002140] text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos voluptates beatae officiis porro ab mollitia quod pariatur autem, dignissimos non expedita iure veniam dicta unde quas, nihil culpa minima reiciendis.</p>
+                        <div className="mt-8">
+                            <div className="flex border-t-2 gap-4 pt-2 pb-2">
+                                <div className="my-auto bg-[#B0F0A5] rounded-md h-14 w-14">
+                                    <svg className="w-14 h-14 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 11.917 9.724 16.5 19 7.5" />
+                                    </svg>
+                                </div>
+                                <p className="ml-2 text-[#002140] text-base">Lorem ipsum dolor situllam saepe blanditiis ipsam molestiae dolorem aliquid ipsum enim ad. Nihil quibusdam aliquam similique obcaecati?</p>
+                            </div>
+                            <div className="flex border-t-2 gap-4 pt-2 pb-2">
+                                <div className="my-auto bg-[#B0F0A5] rounded-md h-14 w-14">
+                                    <svg className="w-14 h-14 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 11.917 9.724 16.5 19 7.5" />
+                                    </svg>
+                                </div>
+                                <p className="ml-2 text-[#002140] text-base">Lorem ipsum dolor situllam saepe blanditiis ipsam molestiae dolorem aliquid ipsum enim ad. Nihil quibusdam aliquam similique obcaecati?</p>
+                            </div>
+                            <div className="flex border-t-2 gap-4 pt-2 pb-2">
+                                <div className="my-auto bg-[#B0F0A5] rounded-md h-14 w-14">
+                                    <svg className="w-14 h-14 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 11.917 9.724 16.5 19 7.5" />
+                                    </svg>
+                                </div>
+                                <p className="ml-2 text-[#002140] text-base">Lorem ipsum dolor situllam saepe blanditiis ipsam molestiae dolorem aliquid ipsum enim ad. Nihil quibusdam aliquam similique obcaecati?</p>
+                            </div>
+                            <div className="flex border-t-2 gap-4 pt-2 pb-2">
+                                <div className="my-auto bg-[#B0F0A5] rounded-md h-14 w-14">
+                                    <svg className="w-14 h-14 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 11.917 9.724 16.5 19 7.5" />
+                                    </svg>
+                                </div>
+                                <p className="ml-2 text-[#002140] text-base">Lorem ipsum dolor situllam saepe blanditiis ipsam molestiae dolorem aliquid ipsum enim ad. Nihil quibusdam aliquam similique obcaecati?</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="ml-20 w-[500px] h-[500px] my-auto rounded-3xl shadow-sm overflow-hidden">
+                        <img className="object-cover w-full h-full" src="Employes-1.jpg" alt="" />
+                    </div>
+                </div>
+
+                <div className="flex w-full justify-center text-[#002140]">
+                    <div className="w-[500px] h-[500px] my-auto rounded-3xl shadow-sm overflow-hidden">
+                        <img className="object-cover w-full h-full" src="Employes-2.jpg" alt="" />
+                    </div>
+                    <div className=" ml-20  w-1/3">
+                        <span className="font-bold text-2xl italic text-[#002140]">Familias</span>
+                        <p className="mt-3 text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos voluptates beatae officiis porro ab mollitia quod pariatur autem, dignissimos non expedita iure veniam dicta unde quas, nihil culpa minima reiciendis.</p>
+                        <div className="mt-8">
+                            <div className="flex border-t-2 gap-4 pt-2 pb-2">
+                                <div className="my-auto bg-[#B0E0DF] rounded-md h-14 w-14">
+                                    <svg className="w-14 h-14 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 11.917 9.724 16.5 19 7.5" />
+                                    </svg>
+                                </div>
+                                <p className="ml-2  text-base">Lorem ipsum dolor situllam saepe blanditiis ipsam molestiae dolorem aliquid ipsum enim ad. Nihil quibusdam aliquam similique obcaecati?</p>
+                            </div>
+                            <div className="flex border-t-2 gap-4 pt-2 pb-2">
+                                <div className="my-auto bg-[#B0E0DF] rounded-md h-14 w-14">
+                                    <svg className="w-14 h-14 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 11.917 9.724 16.5 19 7.5" />
+                                    </svg>
+                                </div>
+                                <p className="ml-2 text-base">Lorem ipsum dolor situllam saepe blanditiis ipsam molestiae dolorem aliquid ipsum enim ad. Nihil quibusdam aliquam similique obcaecati?</p>
+                            </div>
+                            <div className="flex border-t-2 gap-4 pt-2 pb-2">
+                                <div className="my-auto bg-[#B0E0DF] rounded-md h-14 w-14">
+                                    <svg className="w-14 h-14 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 11.917 9.724 16.5 19 7.5" />
+                                    </svg>
+                                </div>
+                                <p className="ml-2 text-base">Lorem ipsum dolor situllam saepe blanditiis ipsam molestiae dolorem aliquid ipsum enim ad. Nihil quibusdam aliquam similique obcaecati?</p>
+                            </div>
+                            <div className="flex border-t-2 gap-4 pt-2 pb-2">
+                                <div className="my-auto bg-[#B0E0DF] rounded-md h-14 w-14">
+                                    <svg className="w-14 h-14 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 11.917 9.724 16.5 19 7.5" />
+                                    </svg>
+                                </div>
+                                <p className="ml-2 text-base">Lorem ipsum dolor situllam saepe blanditiis ipsam molestiae dolorem aliquid ipsum enim ad. Nihil quibusdam aliquam similique obcaecati?</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
