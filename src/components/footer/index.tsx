0f514168f17b2ad23ac2dd93fd8aeb33a5e455bb
Christian Daniel Campaña Díaz <christiand0393@gmail.com>
2024-06-21 00:49:11 -0500

    :bug: fix: footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,4 +1,10 @@
 import AppIco from '../../common/icons/App_Ico';
+import GmailIco from '../../common/icons/gmailIco';
+import PhoneIco from '../../common/icons/phoneIco';
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+import DownloadAS from '../../common/images/downloadAS';
+import DownloadGS from '../../common/images/downloadGS';
 
 const Footer = () => {
   return (
@@ -15,8 +21,30 @@ const Footer = () => {
                 <input className="w-[474px] h-[65px] rounded-[30px]" type="text" placeholder="Ingrese su correo electronico" />
               </div>
               <div className="flex flex-col min-w-[482.5px] justify-between py-5">
-                <div className="text-white">Legales</div>
-                <div className="text-white">Descarga la app</div>
+                <div>
+                  <div className="flex flex-col">
+                    <div className="text-white">
+                      Legales
+                    </div>
+                    <div className="text-white">
+                      Terminos y Condiciones
+                    </div>
+                    <div className="text-white">
+                      Politicas de Privacidad
+                    </div>
+                  </div>
+                </div>
+                <div className="text-white">
+                  <div className="flex flex-col">
+                    <div className="text-white">
+                      Descarga la app
+                    </div>
+                    <div className="flex">
+                      <DownloadGS />
+                      <DownloadAS />
+                    </div>
+                  </div>
+                </div>
                 <button className="w-[180px] bg-[#2F5FE3] h-[65px] rounded-[30px] text-white">Suscribete</button>
               </div>
               <div className="flex flex-col min-w-[482.5px] justify-between py-5">
@@ -27,17 +55,31 @@ const Footer = () => {
                   <span className="text-white">
                     Nuestro canal de soporte
                   </span>
-                  <span className="text-white">
-                    (+51) 958 346 375
+                  <span className="flex gap-4 text-white">
+                    <PhoneIco /> (+51) 958 346 375
                   </span>
                   <span className="text-white">
                     Escribenos por correo electronico
                   </span>
-                  <span className="text-white">
-                    info@inikuna.pe
+                  <span className="flex gap-4 text-white">
+                    <GmailIco /> info@inikuna.pe
                   </span>
                 </div>
-                <div className="text-white">Siguenos en</div>
+                <div className="flex flex-col">
+                  <span className="text-white">
+                    Siguenos en
+                  </span>
+                  <div className="flex flex-row">
+                    <div className="w-10 h-10">
+                      <FaFacebookF 
+                        style={{ color: '#2F5FE3!important' }}
+                      />
+                    </div>
+                    <div className="w-10 h-10">
+                      <FaInstagram />
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
             <hr />
